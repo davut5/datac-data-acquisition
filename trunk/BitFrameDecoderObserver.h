618f6b0b0cbecdb301dc35480847af2b6c0d8756
ahbeezee@gmail.com <ahbeezee@gmail.com@c9afe9ab-c82c-fbd5-357f-0c3ff2792978>
2011-05-10 16:18:37 +0000

    Separated protocol into own file.

---
+++ trunk/BitFrameDecoderObserver.h
@@ -0,0 +1,22 @@
+//
+//  BitFrameDecoderObserver.h
+//  Datac
+//
+//  Created by Brad Howes on 5/10/11.
+//  Copyright 2011 Brad Howes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/** A protocol for observers interested in frame decoding.
+ */
+@protocol BitFrameDecoderObserver
+@required
+
+/** Notification sent out when the bit frame decoder finds a valid message frame.
+ \param buttonState the current button state from the external device (8 bits)
+ \param frequency the current frequency (RPM) reported by the external device (16 bits)
+ */
+- (void)frameButtonState:(NSInteger)buttonState frequency:(NSInteger)frequency;
+
+@end
