4ade9d421bcffe192c419b24c023f2dd1baf7991
ahbeezee <ahbeezee@gmail.com>
2011-05-16 08:02:19 +0000

    Additional icon resources.

---
+++ GuideViewController.mm
@@ -0,0 +1,44 @@
+    //
+//  GuideViewController.mm
+//  Datac
+//
+//  Created by Brad Howes on 5/11/11.
+//  Copyright 2011 Brad Howes. All rights reserved.
+//
+
+#import "GuideViewController.h"
+
+@implementation GuideViewController
+@synthesize url;
+
+- (void)viewDidLoad
+{
+    NSString* file = @"guide.html";
+    NSString* path = [[NSBundle mainBundle] pathForResource:[file stringByDeletingPathExtension] 
+                                                     ofType:[file pathExtension]];
+    self.url = [NSURL fileURLWithPath:path];
+    [super viewDidLoad];
+}
+
+- (void)dealloc
+{
+    self.url = nil;
+    [super dealloc];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [webView loadRequest:[NSURLRequest requestWithURL:self.url]];
+}
+
+- (void)viewDidUnload {
+    self.url = nil;
+    [super viewDidUnload];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES;
+}
+
+@end
